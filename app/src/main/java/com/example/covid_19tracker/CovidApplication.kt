229ae2f88f9a3192e5f02ff3bd4d7671dfc0baac
Manar <manaraelkady@gmail.com>
2020-04-29 21:54:47 +0200

    Add Work Manager Refresher

--- app/src/main/java/com/example/covid_19tracker/CovidApplication.kt
+++ app/src/main/java/com/example/covid_19tracker/CovidApplication.kt
@@ -1,11 +1,43 @@
 package com.example.covid_19tracker
 
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.covid_19tracker.WorkManager.RefreshWorkManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class CovidApplication:Application() {
+
+    private val appScope =  CoroutineScope(Dispatchers.Default)
+    //TODO >> get the setting user defined refresh time from shared preference
+    private var REFRESH_TIME:Long = 2
+    set(value) {
+        REFRESH_TIME = value
+    }
     override fun onCreate() {
+        //Run once application is intialized and before first screen appears
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        initRefreshWorker()
+    }
+    private fun initRefreshWorker(){
+        appScope.launch {
+            setUpRefreshWorker()
+        }
+    }
+    fun setUpRefreshWorker(){
+        val constraints = Constraints.Builder().setRequiresBatteryNotLow(true)
+            .setRequiresDeviceIdle(false).build()
+        val refreshRequest = PeriodicWorkRequestBuilder<RefreshWorkManager>(REFRESH_TIME,TimeUnit.MINUTES)
+            .setConstraints(constraints).build()
+        WorkManager.getInstance().enqueueUniquePeriodicWork(RefreshWorkManager.REFRESH_WORKER
+            ,ExistingPeriodicWorkPolicy.REPLACE,refreshRequest)
     }
 }
