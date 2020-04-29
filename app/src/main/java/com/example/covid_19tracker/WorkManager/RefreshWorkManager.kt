92631208db85486513b218372a09da27a063738e
Manar <manaraelkady@gmail.com>
2020-04-29 19:19:52 +0200

    Intial Worker Manage

--- app/src/main/java/com/example/covid_19tracker/WorkManager/RefreshWorkManager.kt
+++ app/src/main/java/com/example/covid_19tracker/WorkManager/RefreshWorkManager.kt
@@ -1,2 +1,11 @@
 package com.example.covid_19tracker.WorkManager
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class RefreshWorkManager(appContext: Context, params: WorkerParameters): CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+}
