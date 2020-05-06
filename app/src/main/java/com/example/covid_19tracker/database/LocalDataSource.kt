cd566d6ef35963c82f89613252e1d88ee713ec88
Manar <manaraelkady@gmail.com>
2020-05-06 17:33:47 +0200

    Add Subscribe Table, notify Logic

--- app/src/main/java/com/example/covid_19tracker/database/LocalDataSource.kt
+++ app/src/main/java/com/example/covid_19tracker/database/LocalDataSource.kt
@@ -55,7 +55,7 @@ class LocalDataSource(context: Context) : CovidDao {
     }
 
 
-    override fun getCountryByName(countryName: String): LiveData<CountyEntity>? {
+    override fun getCountryByName(countryName: String): LiveData<CountyEntity> {
         return dao.getCountryByName(countryName)
     }
 
